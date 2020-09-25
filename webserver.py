136476d7c14fb404070d3cfa10b674ee3e246b43
Vinh Nguyen <vinh.nguyen@sjsu.edu>
2020-09-25 12:56:51 -0700

    Added webserver.py and HelloWorld.html

---
+++ webserver.py
@@ -0,0 +1,54 @@
+#import socket module
+from socket import *
+import sys # In order to terminate the program
+serverSocket = socket(AF_INET, SOCK_STREAM)
+#Prepare a sever socket
+
+#Fill in start
+serverHost = '127.0.0.1'
+serverPort = 6789
+serverSocket.bind((serverHost,serverPort))
+serverSocket.listen(1)
+#Fill in end
+
+while True:
+	#Establish the connection
+	print('Ready to serve...')
+	connectionSocket, addr = serverSocket.accept()
+	try:
+		message = connectionSocket.recv(1024)
+		filename = message.split()[1]
+		f = open(filename[1:])
+
+
+		outputdata = f.readlines()
+
+		#Send one HTTP header line into socket
+		#Fill in start
+		connectionSocket.send(b"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n")
+		connectionSocket.send(b"\r\n")
+		#Fill in end
+
+		#Send the content of the requested file to the client
+		for i in range(0, len(outputdata)):
+			connectionSocket.send(outputdata[i].encode())
+		connectionSocket.send("\r\n".encode())
+
+		connectionSocket.close()
+
+	except IOError:
+		#Send response message for file not found
+
+		#Fill in start
+		connectionSocket.send(b"HTTP/ 1.1 404 Not Found\r\nContent-Type: text/html\r\n")
+		connectionSocket.send(b"\r\n")
+		connectionSocket.send(b"<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n")
+		#Fill in end
+
+		#Close client socket
+		#Fill in start
+		connectionSocket.close()
+		#Fill in end
+
+serverSocket.close()
+sys.exit()#Terminate the program after sending the corresponding data 
